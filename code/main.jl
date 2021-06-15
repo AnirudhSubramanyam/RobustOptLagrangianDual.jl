9fb27314f9ecfcaa7bda7165733858ffe567ecde
Anirudh Subramanyam <asubramanyam@moonshot.cels.anl.gov>
2021-06-15 10:25:19 -0500

    add network design

--- code/main.jl
+++ code/main.jl
@@ -14,29 +14,41 @@ end
 
 include("problem.jl")
 include("facility.jl")
+include("network.jl")
 
 function test_facility_location(instance::String)
-    budget = 10
-    time_limit = 7200.0
+    budget = 2
+    time_limit = 120.0
     timer = TimerOutput()
     reset_timer!(timer)
 
     problem = FacilityLocation(instance, budget)
-    run_default(problem, KKT, CCG, time_limit, timer)
-    run_default(problem, LinearizedDual, CCG, time_limit, timer)
-    run_default(problem, IndicatorDual, CCG, time_limit, timer)
-    run_default(problem, IndicatorDual, BD, time_limit, timer)
-    #=
-    run_default(problem, IndicatorDual, BD, time_limit, timer)
-    run_default(problem, Penalty, CCG, time_limit, timer)
-    run_default(problem, Penalty, BD, time_limit, timer)
-    run_adaptive_penalty(problem, CCG, time_limit, timer)
-    run_adaptive_penalty(problem, BD, time_limit, timer)
-    =#
-
+    # run_default(problem, KKT, CCG, time_limit, timer)
+    # run_default(problem, IndicatorDual, CCG, time_limit, timer)
+    # run_default(problem, IndicatorDual, BD, time_limit, timer)
+    run_adaptive_penalty_outer(problem, CCG, time_limit, timer)
+    # run_adaptive_penalty_outer(problem, BD, time_limit, timer)
     @show(timer)
 
-    return 0
+    return nothing
 end
 
-test_facility_location("data/CFLP/Cap_F10_C10.txt")
+function test_network_design(instance::String)
+    budget = 1
+    time_limit = 120.0
+    timer = TimerOutput()
+    reset_timer!(timer)
+
+    # det = solve_deterministic_problem(NetworkDesign("data/SNDLIB/dfn-gwin.txt", 1.0, 0)); @show(det);
+
+    problem = NetworkDesign(instance, 1.0, budget)
+    run_default(problem, IndicatorDual, CCG, time_limit, timer)
+    run_adaptive_penalty_outer(problem, CCG, time_limit, timer)
+    @show(timer)
+
+    return nothing
+end
+
+# test_facility_location("data/CFLP/Cap_F20_C25.txt")
+# test_network_design("data/SNDLIB/dfn-bwin.txt")
+# test_network_design("data/SNDLIB/dfn-gwin.txt")
---
+++ code/network.jl
@@ -0,0 +1,326 @@
+struct NetworkDesign <: Problem
+    NumNodes::Int
+    NumEdges::Int
+    Nodes::Vector{String}
+    Edges::Vector{String}
+    Orig::Dict{String,String}
+    Dest::Dict{String,String}
+    PreInstalledCap::Dict{String,Float64}
+    PerUnitCapCost::Dict{String,Float64}
+    PerUnitTrCost::Dict{String,Float64}
+    DisAllowCap::Vector{String}
+    Demand::Dict{String,Float64}
+    Multiplier::Float64
+    budget::Int
+    CompleteRecourse::Bool
+
+    function NetworkDesign(filename::String, Multiplier::Float64, budget::Int)
+        data = readdlm(filename)
+        pos = findall(x->x=="NODES",data); @assert length(pos)==1; (r,c) = Tuple(pos[1]); @assert c==1
+
+        Nodes = Vector{String}()
+        for rowIdx in r+1:size(data,1)
+            if data[rowIdx,1] == ")"
+                break
+            end
+            push!(Nodes, data[rowIdx,1])
+        end
+
+        pos = findall(x->x=="LINKS",data); @assert length(pos)==1; (r,c) = Tuple(pos[1]); @assert c==1
+        Edges = Vector{String}()
+        Orig = Dict{String,String}()
+        Dest = Dict{String,String}()
+        PreInstalledCap = Dict{String,Float64}()
+        PerUnitCapCost = Dict{String,Float64}()
+        PerUnitTrCost = Dict{String,Float64}()
+        DisAllowCap = Vector{String}()
+        for rowIdx in r+1:size(data,1)
+            if data[rowIdx,1] == ")"
+                break
+            end
+            e = data[rowIdx,1]
+            push!(Edges, e)
+            Orig[e] = data[rowIdx,3]
+            Dest[e] = data[rowIdx,4]
+            PreInstalledCap[e] = data[rowIdx,6]
+            if isa(data[rowIdx,11], Number) && isa(data[rowIdx,12], Number)
+                ModuleCap = data[rowIdx,11]
+                ModuleCost = data[rowIdx,12]
+                PerUnitCapCost[e] = ModuleCost/ModuleCap
+                PerUnitTrCost[e] = Multiplier*PerUnitCapCost[e]
+            else
+                PerUnitCapCost[e] = NaN
+                PerUnitTrCost[e] = 0
+                push!(DisAllowCap, e)
+            end
+        end
+
+        pos = findall(x->x=="DEMANDS",data); @assert length(pos)==1; (r,c) = Tuple(pos[1]); @assert c==1
+        Demand = Dict{String,Float64}()
+        for i in Nodes
+            Demand[i] = 0
+        end
+        for rowIdx in r+1:size(data,1)
+            if data[rowIdx,1] == ")"
+                break
+            end
+            src = data[rowIdx,3]
+            tgt = data[rowIdx,4]
+            demand = data[rowIdx,7]
+            Demand[tgt] += demand
+            Demand[src] -= demand
+        end
+        NumNodes = length(Nodes)
+        NumEdges = length(Edges)
+
+        new(NumNodes,
+            NumEdges,
+            Nodes,
+            Edges,
+            Orig,
+            Dest,
+            PreInstalledCap,
+            PerUnitCapCost,
+            PerUnitTrCost,
+            DisAllowCap,
+            Demand,
+            Multiplier,
+            budget,
+            false)
+    end
+end
+
+function solve_deterministic_problem(ND::NetworkDesign)
+    m = initializeJuMPModel()
+
+    @variable(m, u[ND.Edges] >= 0)
+    @variable(m, ff[ND.Edges] >= 0)
+    @variable(m, fb[ND.Edges] >= 0)
+    for e in ND.DisAllowCap
+        fix(u[e], 0.0; force = true)
+    end
+
+    @objective(m, Min,
+        sum((ND.PerUnitCapCost[e]*u[e]) + (ND.PerUnitTrCost[e]*(ff[e] + fb[e])) for e in ND.Edges if e ∉ ND.DisAllowCap)
+    )
+
+    @constraint(m, [e in ND.Edges],
+        ff[e] <= u[e] + ND.PreInstalledCap[e]
+    )
+    @constraint(m, [e in ND.Edges],
+        fb[e] <= u[e] + ND.PreInstalledCap[e]
+    )
+    @constraint(m, [i in ND.Nodes],
+        sum(ff[e] - fb[e] for e in ND.Edges if ND.Dest[e]==i) + sum(fb[e]-ff[e] for e in ND.Edges if ND.Orig[e]==i) == ND.Demand[i]
+    )
+
+    optimize!(m)
+
+    return objective_value(m)
+end
+
+function init_master(ND::NetworkDesign)
+    m = initializeJuMPModel()
+
+    @variable(m, u[ND.Edges] >= 0)
+    for e in ND.DisAllowCap
+        fix(u[e], 0.0; force = true)
+    end
+    @variable(m, s >= 0)
+    @objective(m, Min, s)
+
+    return m
+end
+
+function update_master(ND::NetworkDesign, MP::JuMP.Model, SP::JuMP.Model, master::MasterType, subproblem::SubproblemType)
+    s = MP[:s]
+    u = MP[:u]
+
+    if master == CCG
+        z = JuMP.value.(SP[:z])
+        z = Float64.(Int.(round.(z))) # should be 0-1
+        ff = @variable(MP, [ND.Edges], lower_bound=0)
+        fb = @variable(MP, [ND.Edges], lower_bound=0)
+
+        @constraint(MP, [e in ND.Edges],
+            ff[e] <= (u[e] + ND.PreInstalledCap[e])*(1 - z[e])
+        )
+        @constraint(MP, [e in ND.Edges],
+            fb[e] <= (u[e] + ND.PreInstalledCap[e])*(1 - z[e])
+        )
+        @constraint(MP, [i in ND.Nodes],
+            sum(ff[e] - fb[e] for e in ND.Edges if ND.Dest[e]==i) + sum(fb[e]-ff[e] for e in ND.Edges if ND.Orig[e]==i) == ND.Demand[i]
+        )
+        @constraint(MP,
+            s >= sum((ND.PerUnitCapCost[e]*u[e]) + (ND.PerUnitTrCost[e]*(ff[e] + fb[e])) for e in ND.Edges if e ∉ ND.DisAllowCap)
+        )
+    end
+
+    if master == BD
+        @assert false
+    end
+end
+
+function build_sp_indicator_dual(ND::NetworkDesign, MP::JuMP.Model, feasibility::Bool)
+    u = JuMP.value.(MP[:u])
+
+    SP = initializeJuMPModel()
+    if feasibility
+        @variable(SP, -1 <= μ[ND.Nodes] <= 1)
+        @variable(SP, -1 <= vb[ND.Edges] <= 0)
+        @variable(SP, -1 <= vf[ND.Edges] <= 0)
+    else
+        @variable(SP, μ[ND.Nodes])
+        @variable(SP, vb[ND.Edges] <= 0)
+        @variable(SP, vf[ND.Edges] <= 0)
+    end
+    @variable(SP, zb[ND.Edges] <= 0)
+    @variable(SP, zf[ND.Edges] <= 0)
+    @variable(SP, z[ND.Edges], Bin)
+
+    # objective
+    if feasibility
+        @objective(SP, Max,
+            +sum((ND.PreInstalledCap[e]+u[e])*(vb[e]-zb[e]) for e in ND.Edges if e ∉ ND.DisAllowCap)
+            +sum((ND.PreInstalledCap[e]+u[e])*(vf[e]-zf[e]) for e in ND.Edges if e ∉ ND.DisAllowCap)
+            +sum(ND.Demand[i]*μ[i] for i in ND.Nodes)
+        )
+    else
+        @objective(SP, Max,
+            +sum((ND.PerUnitCapCost[e]*u[e]) for e in ND.Edges if e ∉ ND.DisAllowCap)
+            +sum((ND.PreInstalledCap[e]+u[e])*(vb[e]-zb[e]) for e in ND.Edges if e ∉ ND.DisAllowCap)
+            +sum((ND.PreInstalledCap[e]+u[e])*(vf[e]-zf[e]) for e in ND.Edges if e ∉ ND.DisAllowCap)
+            +sum(ND.Demand[i]*μ[i] for i in ND.Nodes)
+        )
+    end
+
+    # uncertainty set
+    @constraint(SP, sum(z[e] for e in ND.Edges) <= ND.budget)
+    @constraint(SP, [i in ND.Nodes; abs(ND.Demand[i]) > 1e-3],
+        sum(z[e]-1 for e in ND.Edges if ND.Dest[e] == i) + sum(z[e]-1 for e in ND.Edges if ND.Orig[e] == i) <= - 1
+    )
+
+
+    # dual feasibility
+    @constraint(SP, [e in ND.Edges],
+        vb[e] + μ[ND.Orig[e]] - μ[ND.Dest[e]] <= (feasibility ? 0 : ND.PerUnitTrCost[e])
+    )
+    @constraint(SP, [e in ND.Edges],
+        vf[e] - μ[ND.Orig[e]] + μ[ND.Dest[e]] <= (feasibility ? 0 : ND.PerUnitTrCost[e])
+    )
+
+    # indicators
+    @constraint(SP, [e in ND.Edges],
+        !z[e] => {zb[e] >= 0}
+    )
+    @constraint(SP, [e in ND.Edges],
+        z[e] => {zb[e] == vb[e]}
+    )
+    @constraint(SP, [e in ND.Edges],
+        !z[e] => {zf[e] >= 0}
+    )
+    @constraint(SP, [e in ND.Edges],
+        z[e] => {zf[e] == vf[e]}
+    )
+
+    return SP
+end
+
+function build_sp_fixed_penalty(ND::NetworkDesign, MP::JuMP.Model, rho::Float64, feasibility::Bool)
+    u = JuMP.value.(MP[:u])
+
+    SP = initializeJuMPModel()
+    if feasibility
+        @variable(SP, -1 <= μ[ND.Nodes] <= 1)
+        @variable(SP, -1 <= vb[ND.Edges] <= 0)
+        @variable(SP, -1 <= vf[ND.Edges] <= 0)
+    else
+        @variable(SP, μ[ND.Nodes])
+        @variable(SP, vb[ND.Edges] <= 0)
+        @variable(SP, vf[ND.Edges] <= 0)
+    end
+    @variable(SP, z[ND.Edges], Bin)
+    
+    # objective
+    if feasibility
+        if solver == "Gurobi"
+            JuMP.set_optimizer_attribute(SP, "SolutionLimit", 1)
+            JuMP.set_optimizer_attribute(SP, "Cutoff", 1e-4)
+        end
+        @objective(SP, Max,
+            +sum((ND.PreInstalledCap[e]+u[e])*vb[e] for e in ND.Edges if e ∉ ND.DisAllowCap)
+            +sum((ND.PreInstalledCap[e]+u[e])*vf[e] for e in ND.Edges if e ∉ ND.DisAllowCap)
+            +sum(ND.Demand[i]*μ[i] for i in ND.Nodes)
+        )
+    else
+        @objective(SP, Max,
+            +sum((ND.PerUnitCapCost[e]*u[e]) for e in ND.Edges if e ∉ ND.DisAllowCap)
+            +sum((ND.PreInstalledCap[e]+u[e])*vb[e] for e in ND.Edges if e ∉ ND.DisAllowCap)
+            +sum((ND.PreInstalledCap[e]+u[e])*vf[e] for e in ND.Edges if e ∉ ND.DisAllowCap)
+            +sum(ND.Demand[i]*μ[i] for i in ND.Nodes)
+        )
+    end
+
+    # uncertainty set
+    @constraint(SP, sum(z[e] for e in ND.Edges) <= ND.budget)
+    @constraint(SP, [i in ND.Nodes; abs(ND.Demand[i]) > 1e-3],
+        sum(z[e]-1 for e in ND.Edges if ND.Dest[e] == i) + sum(z[e]-1 for e in ND.Edges if ND.Orig[e] == i) <= - 1
+    )
+
+    # dual feasibility
+    @constraint(SP, [e in ND.Edges],
+        vb[e] + μ[ND.Orig[e]] - μ[ND.Dest[e]] <= (feasibility ? 0 : ND.PerUnitTrCost[e]) + (rho*z[e])
+    )
+    @constraint(SP, [e in ND.Edges],
+        vf[e] - μ[ND.Orig[e]] + μ[ND.Dest[e]] <= (feasibility ? 0 : ND.PerUnitTrCost[e]) + (rho*z[e])
+    )
+
+    return SP
+end
+
+function build_sp(ND::NetworkDesign, MP::JuMP.Model, subproblem::SubproblemType, rho::Float64 = 1.0, feasibility::Bool=true)
+    if subproblem == KKT
+        @assert false
+    end
+
+    if subproblem == Penalty
+        return build_sp_fixed_penalty(ND, MP, rho, feasibility)
+    end
+
+    if subproblem == LinearizedDual
+        @assert false
+    end
+
+    if subproblem == IndicatorDual
+        return build_sp_indicator_dual(ND, MP, feasibility)
+    end
+end
+
+function solve_second_stage_problem_penalty(ND::NetworkDesign, MP::JuMP.Model, SP::JuMP.Model, rho::Float64)
+    u = JuMP.value.(MP[:u])
+    z = JuMP.value.(SP[:z])
+    z = Float64.(Int.(round.(z))) # should be 0-1
+    
+    m = initializeJuMPModel()
+    @variable(m, ff[ND.Edges] >= 0)
+    @variable(m, fb[ND.Edges] >= 0)
+
+    @objective(m, Min,
+        +sum((ND.PerUnitCapCost[e]*u[e]) + (ND.PerUnitTrCost[e]*(ff[e] + fb[e])) for e in ND.Edges if e ∉ ND.DisAllowCap)
+        +sum(rho*z[e]*(ff[e] + fb[e]) for e in ND.Edges)
+    )
+
+    @constraint(m, [e in ND.Edges],
+        ff[e] <= u[e] + ND.PreInstalledCap[e]
+    )
+    @constraint(m, [e in ND.Edges],
+        fb[e] <= u[e] + ND.PreInstalledCap[e]
+    )
+    @constraint(m, [i in ND.Nodes],
+        sum(ff[e] - fb[e] for e in ND.Edges if ND.Dest[e]==i) + sum(fb[e]-ff[e] for e in ND.Edges if ND.Orig[e]==i) == ND.Demand[i]
+    )
+
+    optimize!(m)
+    step = sum(z[e]*(value(ff[e]) + value(fb[e])) for e in ND.Edges)
+    return step
+end
