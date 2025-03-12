cd5bfcb228799deee3b24aae869a8a0521953a63
Anirudh Subramanyam <anirudh@Anirudhs-MacBook-Pro.local>
2025-03-12 15:20:17 -0400

    update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -6,7 +6,6 @@ makedocs(
         prettyurls=Base.get(ENV, "CI", nothing) == "true",
         mathengine=Documenter.KaTeX()
     ),
-    strict=true,
     pages=[
         "Home" => "index.md",
         "Library" => [
